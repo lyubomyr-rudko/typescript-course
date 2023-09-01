306696f166e3a4895b20b18fa00d8aaba9b7a683
Danylo Dovban <epumbar@gmail.com>
2023-09-01 15:00:51 +0300

    Homework 2

--- src/lesson1-homework.ts
+++ src/lesson1-homework.ts
@@ -49,43 +49,43 @@ excercise3();
 
 ///////////////////////////////////////////////////
 
-function exercise4() {
-  let one: [number, number] = [1, 1];
-  let two: [number, number] = [4, 5];
+// function exercise4() {
+//   let one: [number, number] = [1, 1];
+//   let two: [number, number] = [4, 5];
 
-  function distance(p1: [number, number], p2: [number, number]): number {
-    const x1 = p1[0]; // TODO: replace with the first element of p1
-    const y1 = p1[1]; // TODO: replace with the second element of p1
-    const x2 = p2[0]; // TODO: replace with the first element of p2
-    const y2 = p2[1]; // TODO: replace with the second element of p2
+//   function distance(p1: [number, number], p2: [number, number]): number {
+//     const x1 = p1[0]; // TODO: replace with the first element of p1
+//     const y1 = p1[1]; // TODO: replace with the second element of p1
+//     const x2 = p2[0]; // TODO: replace with the first element of p2
+//     const y2 = p2[1]; // TODO: replace with the second element of p2
 
-    return Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
-  }
+//     return Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
+//   }
 
-  const res = distance(one, two);
+//   const res = distance(one, two);
 
-  console.log(res);
-}
-exercise4();
+//   console.log(res);
+// }
+// exercise4();
 
 /////////////////////////////////////////////////////////////////////
 
-function exercise5() {
-  type TPoint = { x: number; y: number };
+// function exercise5() {
+//   type TPoint = { x: number; y: number };
 
-  const one: TPoint = { x: 1, y: 1 };
-  const two: TPoint = { x: 4, y: 5 };
+//   const one: TPoint = { x: 1, y: 1 };
+//   const two: TPoint = { x: 4, y: 5 };
 
-  function distance(p1: TPoint, p2: TPoint): number {
-    const { x: x1, y: y1 } = p1; // Destructuring assignment for point 1
-    const { x: x2, y: y2 } = p2; // Destructuring assignment for point 2
+//   function distance(p1: TPoint, p2: TPoint): number {
+//     const { x: x1, y: y1 } = p1; // Destructuring assignment for point 1
+//     const { x: x2, y: y2 } = p2; // Destructuring assignment for point 2
 
-    const dx = x2 - x1;
-    const dy = y2 - y1;
-    return Math.sqrt(dx ** 2 + dy ** 2);
-  }
+//     const dx = x2 - x1;
+//     const dy = y2 - y1;
+//     return Math.sqrt(dx ** 2 + dy ** 2);
+//   }
 
-  const result = distance(one, two);
-  console.log(result);
-}
-excercise5();
+//   const result = distance(one, two);
+//   console.log(result);
+// }
+// excercise5();

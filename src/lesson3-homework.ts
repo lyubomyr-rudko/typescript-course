856572641a34252c935538460d5ef2be509379d9
Victor Davydenko <acc.davydenko@gmail.com>
2023-09-07 17:05:36 +0300

    hw3 done

--- src/lesson3-homework.ts
+++ src/lesson3-homework.ts
@@ -1,5 +1,5 @@
 // try different target compiler options
-function excercise10() {
+function excercise10(): void {
   // TODO: declare a Rectangle class, with width and height properties
   // TODO: add a constructor which takes width and height as parameters
   // TODO: add a method `getArea` which returns the area of the rectangle
@@ -14,12 +14,31 @@ function excercise10() {
   // TODO: change width and height properties to be prefixed with #, to use ESNext private fields support
   // TODO: change compiler target to ESNext, complile and see the compiled code
   // TODO: change compiler target to ES5, try to compile, check if you get the error Private identifiers are only available when targeting ECMAScript 2015 and higher.(18028)
+  class Rectangle {
+    #width: number;
+    #height: number;
+    constructor(width: number, height: number) {
+      this.#width = width;
+      this.#height = height;
+    }
+
+    getArea(): number {
+      return this.#width * this.#height;
+    }
+
+    getPerimeter(): number {
+      return (this.#width + this.#height) * 2;
+    }
+  }
+  const firstRectangle = new Rectangle(10, 20);
+  console.log(firstRectangle.getArea());
+  console.log(firstRectangle.getPerimeter());
 }
 // TODO: compile and run the code
 excercise10();
 
 // create a generic Stack class (Stack is a FILO data structure, push and pop methods are used to add and remove items from the top of the stack)
-function excercise11() {
+function excercise11(): void {
   // TODO: create a generic Stack class
   // TODO: add a private data property of type array of T
   // TODO: add a push method which takes an item of type T as a parameter and adds it to the top of the stack
@@ -30,30 +49,58 @@ function excercise11() {
   // TODO: create an instance of the Stack class with string type
   // TODO: push two strings to the stack
   // TODO: pop an item from the stack and print it to console, calling toUpperCase method on it
+  class Stack<T> {
+    private data: T[] = []
+
+    push(item: T): void {
+      this.data.push(item);
+    }
+
+    pop(): T | undefined {
+      return this.data.pop()
+    }
+  }
+
+  const instanceWithNumbers: Stack<number> = new Stack()
+  instanceWithNumbers.push(1);
+  instanceWithNumbers.push(3);
+  console.log(instanceWithNumbers.pop()?.toFixed())
+
+  const instanceWithStrings: Stack<string> = new Stack()
+  instanceWithStrings.push("1");
+  instanceWithStrings.push("3");
+  console.log(instanceWithStrings.pop()?.toUpperCase())
 }
 // TODO: compile and run the code
 excercise11();
 
 // create a generic function which takes an array of items of type T and returns the random item from the array
-function excercise12() {
+function excercise12(): void {
   // TODO: create a function that takes an array of numbers and returns a random number from the array
   // TODO: create a function that takes an array of strings and returns a random string from the array
   // TODO: create a function that takes an array of objects and returns a random object from the array
   // TODO: observe the same structure of the functions above, and create a generic function which takes an array of items of type T and returns the random item from the array
+  const arrayOfNumbersFunc = (arr: number[]):number => arr[Math.floor(Math.random() * arr.length)];
+  const arrayOfStringsFunc = (arr: string[]):string => arr[Math.floor(Math.random() * arr.length)];
+  const arrayOfObjectsFunc = (arr: object[]):object => arr[Math.floor(Math.random() * arr.length)];
+  const genericFunc = <T>(arr: T[]): T => arr[Math.floor(Math.random() * arr.length)];
+  console.log(genericFunc<number>([1, 4, 5, 6]));
+  console.log(genericFunc<string>(["1", "4", "5", "6"]));
+  console.log(genericFunc<object>([{name: 'John'}, {name: 'Garry'}]));
 }
 // TODO: compile and run the code
 excercise12();
 
 // add type assertion to the code
-function excercise13() {
+function excercise13(): void {
   // NOTE: do not change this function
   function fetchUserAge(): unknown {
     const responseText = '{"name": "John", "age": 18}';
     return JSON.parse(responseText).age;
   }
-  const userAge = fetchUserAge();
+  const userAge = fetchUserAge() as number;
   // TODO: uncomment the following code and add type assertion to fix the error
-  // console.log(userAge + 1);
+  console.log(userAge + 1);
 }
 // TODO: compile and run the code
 excercise13();
@@ -66,7 +113,7 @@ function excercise14() {
 
     return JSON.parse(responseText).age;
   }
-  const userAge = fetchUserAge();
+  const userAge = +fetchUserAge() as number;
   // TODO: run the code below and observe the result, explain why it is happening,
   // TODO: add type casting to the function above, to fix the error
   if (userAge === 16) {
@@ -81,29 +128,35 @@ function excercise14() {
 excercise14();
 
 // add type safety to the code which uses any
-function excercise15() {
+function excercise15(): void {
   // TODO: declare a type for user object, which has a name property of type string
 
   // TODO: fix the fetchUsers function to return an array of users, not any type
-  function fetchUsers() {
+
+  type TUser = {name: string};
+  type TUsersResponse = {users: TUser[]}
+  function fetchUsers(): TUser[] {
     // TODO: add type safety to the data variable, annotate it with the type of users
-    const data: unknown = JSON.parse(
+    const data: TUsersResponse = JSON.parse(
       '{"users": [{"name": "John"}, {"name": "Jane"}]}'
     );
 
     // TODO: add check for the data type to contain list of users
-    return data;
+    if (typeof data === 'object' && data !== null && "users" in data ) {
+      return data.users
+    }
+    return [];
   }
   // TODO: fix typings of the users variable (currently it is of type any)
-  const users = fetchUsers();
+  const users: TUser[] = fetchUsers();
   // TODO: add type safety to the code to print the names of the users to console
-  // users.forEach((user) => console.log(user.name));
+  users.forEach((user) => console.log(user.name));
 }
 // TODO: compile and run the code
 excercise15();
 
 // use type declarations to fix the comments in the code
-function excercise16() {
+function excercise16(): void {
   // TODO: add code which uses process.env.NODE_ENV variable,
   // TODO: try to compile and see the error
   // TODO: add type declaration for process.env.NODE_ENV variable in global.d.ts file
@@ -111,6 +164,9 @@ function excercise16() {
   // TODO: remove global.d.ts file, copile and see the error again
   // TODO: install type declarations from error message -  @types/node
   // NOTE: For the most part, type declaration packages should always have the same name as the package name on npm, but prefixed with @types/
+  if (process.env.NODE_ENV === 'dev') {
+    console.log('you are in a dev mode');
+  }
 }
 // TODO: compile and run the code
 excercise16();

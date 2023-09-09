0451089f360a45e2637ad14e4e628746b0d7812b
Victor Davydenko <acc.davydenko@gmail.com>
2023-09-09 14:33:18 +0300

    hw4 done

--- src/lesson4-homework.ts
+++ src/lesson4-homework.ts
@@ -1,4 +1,6 @@
 // add readonly modifier to prevent props reassignment
+import {log} from "util";
+
 function exercise17() {
   // TODO: define class Student with properties name, age, studentId
   // TODO: add constructor to initialize the properties
@@ -7,6 +9,18 @@ function exercise17() {
   // TODO: print the student info to console
   // TODO: try to change the studentId property
   // TODO: change the studentId property to readonly, make sure that changing the property is not allowed
+  class Student {
+    constructor(public name: string, public age: number, readonly studentId: string) {
+    }
+
+    printStudent() {
+      console.log(`Student's ${this.studentId} name is ${this.name} and student is ${this.age} years old` )
+    }
+  }
+
+  const student = new Student('John', 19, 'KT34242');
+  student.printStudent();
+  // student.studentId = 'TT344324'; it's impossible due to readonly property
 }
 // TODO: compile and run the code
 exercise17();
@@ -17,37 +31,38 @@ function exercise18() {
     name: string;
     width: number;
     height: number;
-    color: string;
-    os: string;
+    color?: string;
+    os?: string;
     space: number;
   };
 
   // TODO: uncomment the code below and update the type definition to fix compile time error
 
-  // const widgetWithSize: TWidget = {
-  //   name: 'widget',
-  //   width: 10,
-  //   height: 20,
-  //   color: 'red',
-  //   space: 100,
-  // }
+  const widgetWithSize: TWidget = {
+    name: 'widget',
+    width: 10,
+    height: 20,
+    color: 'red',
+    space: 100,
+  }
 
-  // const desktopWidget:TWidget = {
-  //   name: 'widget',
-  //   width: 10,
-  //   height: 20,
-  //   os: 'windows',
-  //   space: 100,
-  // }
+  const desktopWidget:TWidget = {
+    name: 'widget',
+    width: 10,
+    height: 20,
+    os: 'windows',
+    space: 100,
+  }
 
   // TODO: print the result to console
+  console.log(widgetWithSize, desktopWidget)
 }
 // TODO: compile and run the code
 exercise18();
 
 // use uniton types to replace unknown type for compile time type checking
 function exercise19() {
-  function formatCommandLine(command: unknown) {
+  function formatCommandLine(command: string | string[]) {
     if (typeof command === "string") {
       return command.trim();
     } else if (Array.isArray(command)) {
@@ -58,7 +73,7 @@ function exercise19() {
 
   console.log(formatCommandLine("  git status  ")); // git status
   console.log(formatCommandLine(["git ", " status "])); // git status
-  console.log(formatCommandLine(false)); // run time error - should be compile time error instead
+  // console.log(formatCommandLine(false)); // error because boolean is not assignable to string or array of strings
 }
 // TODO: compile and run the code
 exercise19();
@@ -67,7 +82,8 @@ exercise19();
 function exercise20() {
   // TODO: define rock, paper, scissors literal type and assign it to TMove type
   // TODO: add type check to the function below
-  function rockPaperSizorsVins(me: unknown, other: unknown) {
+  type TMove = 'rock' | 'paper' | 'scissors';
+  function rockPaperSizorsVins(me: TMove, other: TMove) {
     if (me === "rock" && other === "paper") {
       return false;
     }
@@ -84,7 +100,7 @@ function exercise20() {
   console.log(rockPaperSizorsVins("scissors", "rock")); // false
   console.log(rockPaperSizorsVins("rock", "scissors")); // true
   // TODO: make sure that the following calls are not allowed
-  console.log(rockPaperSizorsVins("papapaper", "scissors")); // true - no type check
+  // console.log(rockPaperSizorsVins("papapaper", "scissors")); // papapaper is not assignable to TMove
 }
 // TODO: compile and run the code
 exercise20();
@@ -97,26 +113,17 @@ function exercise21() {
     name: string;
   };
 
-  type TWidgetWithSize = {
-    name: string;
+  type TWidgetWithSize = TWidget & {
     width: number;
     height: number;
     color: string;
   };
 
-  type TDesktopWidget = {
-    name: string;
-    width: number;
-    height: number;
-    color: string;
+  type TDesktopWidget = TWidgetWithSize & {
     os: string;
   };
 
-  type TMobileWidget = {
-    name: string;
-    width: number;
-    height: number;
-    color: string;
+  type TMobileWidget = TWidgetWithSize & {
     space: number;
   };
 
@@ -182,22 +189,17 @@ exercise21();
 
 // rewrite the code using async await
 function exercise22() {
-  function printMessagesWithTimeout() {
-    setTimeout(() => {
-      console.log("1");
+  async function printMessagesWithTimeout() {
+    const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
-      setTimeout(() => {
-        console.log("2");
-      }, 1000);
-
-      setTimeout(() => {
-        console.log("3");
-
-        setTimeout(() => {
-          console.log("4");
-        }, 1000);
-      }, 1000);
-    }, 1000);
+    await delay(1000);
+    console.log(1);
+    await delay(1000);
+    console.log(2)
+    await delay(1000);
+    console.log(3);
+    await delay(1000);
+    console.log(4);
   }
   printMessagesWithTimeout();
 }

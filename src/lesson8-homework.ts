d9d939802731148a3997b806a426e156b0990cec
Philip Harmash <infilya@ukr.net>
2023-09-25 09:18:44 +0300

    Homework #8 Done Harmash

--- src/lesson8-homework.ts
+++ src/lesson8-homework.ts
@@ -13,7 +13,7 @@ function exercise40() {
   }
 
   // TODO: fix the type of fetchResult variable to be union of array of GroupDocument objects / null
-  let fetchResult: any = null;
+  let fetchResult: GroupDocument[] | null = null;
 
   // TODO: keep this code as is
   fetchResult = [
@@ -49,18 +49,18 @@ function exercise40() {
   if (fetchResult !== null) {
     // NOTE: observe taht type narrowing works here
     console.log(fetchResult.length);
+    const localFetchResult = fetchResult;
 
     userNames.forEach((name) => {
       // TOOD: explain why type narrowing does not work here and fix the error (and remove any type annotations)
-      let result = fetchResult.find((obj: any) => obj.name === name);
-
+      let result = localFetchResult.find((obj: GroupDocument) => obj.name === name);
       if (result) {
         console.log(result.data);
       }
     });
   }
 }
-exercise40();
+// exercise40();
 
 // Use typeof operator
 function exercise41() {
@@ -129,9 +129,14 @@ function exercise41() {
   };
 
   // TODO: for each property of the user object, print its type using js typeof operator
+  type TUser = typeof user;
+  type TUserKey = keyof TUser;
+
   function printAllUserPropTypes() {
-    // TODO: get lis of own keys of the user object
+    // TODO: get list of own keys of the user object
+    const keys = Object.keys(user) as TUserKey[];
     // TODO: iterate over the keys with foreach
+    keys.forEach((key: TUserKey) => console.log(typeof user[key]));
     // TODO: console.log the typeof for each property
   }
   printAllUserPropTypes();
@@ -167,7 +172,7 @@ function exercise42() {
           title: "user photo 1",
         },
         {
-          utl: "https://i.dummyjson.com/data/products/1/2.jpg",
+          url: "https://i.dummyjson.com/data/products/1/2.jpg",
           title: "user photo 2",
         },
       ],
@@ -192,15 +197,39 @@ function exercise42() {
   type TProduct = {
     id: number;
     title: string;
-    // TOOD: add remaining missing properties
+    description: string;
+    price: number;
+    discountPercentage: number;
+    rating: number;
+    stock: number;
+    brand: string;
+    category: string;
+    thumbnail: string;
+    images: {
+      url: string;
+      title: string;
+    }[];
+    warehouse: {
+      address: {
+        address: string;
+        city: string;
+        coordinates: {
+          lat: number;
+          lng: number;
+        };
+        postalCode: string;
+        state: string;
+      };
+      name: string;
+      phoneNumbers: string[];
+    };
   };
-
   // TODO: create a type TCoodinates that represents coordinates, using lookup type
   //  (product->warehouse->address->coordinates)
-  type TCoordinates = {};
+  type TCoordinates = TProduct["warehouse"]["address"]["coordinates"];
 
   // TODO: fix/add type annotation for the function (remove any type annotation)
-  function printProductLocationCoordinates(coordinates: TCoordinates | any) {
+  function printProductLocationCoordinates(coordinates: TCoordinates) {
     // NOTE: this could be using google map api to display the location on the map, but for now just console.log
     console.log(coordinates.lat);
     console.log(coordinates.lng);
@@ -209,31 +238,33 @@ function exercise42() {
   printProductLocationCoordinates(products[0].warehouse.address.coordinates);
 
   // you also need a function which returns a phone number of given product's warehouse
+  type TPhoneNumber = TProduct["warehouse"]["phoneNumbers"][0];
   // TODO: add return type annotation using lookup type, instead of hardcoded string type
-  function getProductWarehousePhoneNumber(product: TProduct): string {
+  function getProductWarehousePhoneNumber(product: TProduct): TPhoneNumber {
     // TODO: fix the return value to be a type of a phone number for the product warehouse
     // HINT: use lookup types, and the result for that should equal to string type
     // TODO: make sure the function gets a phone number from product object
-    return "";
+    return product.warehouse.phoneNumbers[0];
   }
 
-  getProductWarehousePhoneNumber(products[0]);
+  console.log(getProductWarehousePhoneNumber(products[0]));
 }
 exercise42();
 
 // Use keyof type operators
 function exercise43() {
   // TODO: implement functions to get and set property of an object in type safe way
-
+  type TPerson = typeof user;
+  type TKeyOfPerson = keyof TPerson;
   // TODO: for type sefty use generics and keyof type operator to ensure that key is a valid property of the object
-  function getProperty(obj: any, key: string) {
+  function getProperty<T>(obj: T, key: keyof T) {
     console.log("getProperty", obj[key]);
 
     return obj[key];
   }
 
   // TODO: use generics and lookup type, add types T, K and use T[K] for value param type
-  function setProperty(obj: any, key: string, value: any) {
+  function setProperty<T, K extends keyof T>(obj: T, key: K, value: T[K]) {
     obj[key] = value;
     console.log("setProperty", obj, key, obj[key]);
   }
@@ -253,36 +284,36 @@ function exercise44() {
   // TODO: create a coditional type that will check if the type is a primitive type (unites all string, number, boolean)
   // TODO: if the type is primitive, return literal type 'primitive'
   // TODO: if the type is not primitive, return literal type 'not primitive'
-  type TIsPrimitive = {};
+  type TIsPrimitive<T> = T extends string | number | boolean ? "primitive" : "not primitive";
 
   // TODO uncomment the following lines
-  //   type T1 = TIsPrimitive<number>; // hint: should be 'primitive'
-  //   type T2 = TIsPrimitive<string>;
-  //   type T3 = TIsPrimitive<0>;
-  //   type T4 = TIsPrimitive<{}>;  // hint: should be 'not primitive'
-  //   type T4 = TIsPrimitive<Function>;  // hint: should be 'not primitive'
+  type T1 = TIsPrimitive<number>; // hint: should be 'primitive'
+  type T2 = TIsPrimitive<string>;
+  type T3 = TIsPrimitive<0>;
+  type T4 = TIsPrimitive<{}>; // hint: should be 'not primitive'
+  type T5 = TIsPrimitive<Function>; // hint: should be 'not primitive'
 }
 exercise44();
 
 // Use conditional types with unions and never
 function exercise45() {
   // TODO: create a type that excludes number from a union type
-  type ExcludeNumberFromType<T> = T extends number ? "number" : "not number"; // TODO: replace with your code
+  type ExcludeNumberFromType<T> = T extends number ? never : T; // TODO: replace with your code
 
   type TNumberOrString = number | string;
 
   type TExcludeNumberFromType = ExcludeNumberFromType<TNumberOrString>; // Hint - should equal to string
 
   // TODO: uncomment the following lines and make sure there are no errors
-  //   const a: TExcludeNumberFromType = "test";
-  //   console.log(a);
+  const a: TExcludeNumberFromType = "test";
+  console.log(a);
 }
 exercise45();
 
 // Use infer keyword
 function exercise46() {
   // create a type that extracts the type of the first argument of a function
-  // type FirstParameter<T> = ...
+  type FirstParameter<T> = T extends (...args: infer R) => any ? R[0] : never;
   function createUser(firstName: string, lastName: string, age: number) {
     const id = (Math.random() * 100000).toString();
 
@@ -294,7 +325,7 @@ function exercise46() {
     };
   }
   // TODO: uncomment the following line and fix the error
-  // type TCreateUserFirstArg = FirstParameter<typeof createUser>; // string
+  type TCreateUserFirstArg = FirstParameter<typeof createUser>; // string
 }
 exercise46();
 
@@ -303,12 +334,17 @@ function exerciseExtra1() {
   // TODO: create a function to determine if two strings are an anagram
   // HINT: A word is an anagram of another if you can rearrange its characters to produce the second word.
   function areAnagrams(s1: string, s2: string): boolean {
+    if (s1.length !== s2.length) {
+      return false;
+    } else if (s1.split("").sort().join() === s2.split("").sort().join()) {
+      return true;
+    }
     return false;
   }
 
-  console.assert(areAnagrams("listen", "silent") === true);
-  console.assert(areAnagrams("abc", "cba") === true);
-  console.assert(areAnagrams("abc", "cbd") === false);
+  console.assert(areAnagrams("listen", "silent"), "listen and silent are not anagrams");
+  console.assert(areAnagrams("abc", "cba"), "abc and cba are not anagrams");
+  console.assert(areAnagrams("abc", "cbd"), "abc and cbd are not anagrams");
 
   // HINT: consider exercise33 for code reuse ideas
 }

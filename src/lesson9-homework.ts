100ff236ab36f2a76a1b242c9ee1a7d2d583d93d
ViktorKW <viktorkw1201@gmail.com>
2023-09-28 14:58:52 +0300

    completed hw

--- src/lesson9-homework.ts
+++ src/lesson9-homework.ts
@@ -1,27 +1,27 @@
-// Use mappping types
+// Use mapping types
 function exercise47() {
   // implement mapped type that takes two types T and K
   // K must be a union of strings or numbers or symbols
   // the mapped type should create a new type that has all properties included in list K, and the value of each property is T
-  // type TRecord<K extends ..., T> = {
-  //   [... in ...]: ...;
-  // };
+  type TRecord<K extends string | number | symbol, T> = {
+    [P in K]: T;
+  };
   // TODO: uncomment the following code and check if your mapped type works
-  // type TPoint = TRecord<"x" | "y" | "z", number>;
-  // const point: TPoint = {
-  //   x: 1,
-  //   y: 2,
-  //   z: 3,
-  // };
+  type TPoint = TRecord<"x" | "y" | "z", number>;
+  const point: TPoint = {
+    x: 1,
+    y: 2,
+    z: 3,
+  };
 }
 exercise47();
 
-// Use mappping types modifiers
+// Use mapping types modifiers
 function exercise48() {
   // implement mapped type that makes all properties of T optional and nullable
-  // type TPartialNullable<T> = {
-  //   [... in ...]: ...;
-  // };
+  type TPartialNullable<T> = {
+    [P in keyof T]+?: T[P] | null;
+  };
 
   type TPoint = {
     x: number;
@@ -30,39 +30,41 @@ function exercise48() {
     name: string;
   };
 
-  // type TNullablePoint = TPartialNullable<TPoint>;
-  // const p1: TNullablePoint = { x: 10 };
-  // const p2: TNullablePoint = { x: 10, y: null };
+  type TNullablePoint = TPartialNullable<TPoint>;
+  const p1: TNullablePoint = { x: 10 };
+  const p2: TNullablePoint = { x: 10, y: null };
 }
 exercise48();
 
 // Template Literal Type
 function exercise49() {
-  // TODO: create a type that represents a string that contains Tshirts sizes (S, M, L, XL, XXL)
-  // TODO: create a type that represents a string that contains Tshirts colors (red, green, blue)
-  // TODO: create a type that represents a string that contains Tshirts sizes and colors (e.g. "S-red", "M-green", "L-blue")
-  // TODO: create a function that takes a size and a color and returns a Tshirt size and color
-  // TOOD: make sure you annotate the params and return type of the function
-  // type TSize =
-  // type TColor =
-  // type TTshirt =
-  // function createTshirt(size, color) {
-  //   return `${size}-${color}`;
-  // }
-  // const tshirt = createTshirt("S", "red");
+  // TODO: create a type that represents a string that contains TShirts sizes (S, M, L, XL, XXL)
+  type TShirtSizes = "S" | "M" | "L" | "XL" | "XXL"
+  // TODO: create a type that represents a string that contains TShirts colors (red, green, blue)
+  type TShirtColors = "red" | "green" | "blue"
+  // TODO: create a type that represents a string that contains TShirts sizes and colors (e.g. "S-red", "M-green", "L-blue")
+  type TShirtDescription = `${TShirtSizes}-${TShirtColors}`
+  // TODO: create a function that takes a size and a color and returns a TShirt size and color
+  function createTShirt(size:TShirtSizes, color:TShirtColors):TShirtDescription{
+    return `${size}-${color}` //wow
+  }
+
+  // TODO: make sure you annotate the params and return type of the function
+  const tShirt = createTShirt("S", "red");
 }
 exercise49();
 
-// Fix autocoplete problem for literal union types
+// Fix autocomplete problem for literal union types
 function exercise50() {
   // TODO: observe the problem with autocomplete in the line createCar("BMW");
   // TODO: fix the problem by using the approach from the lesson
-  type Brands = "BMW" | "Mercedes" | "Audi" | string;
+  type Brands = "BMW" | "Mercedes" | "Audi" | (string & {});
 
   function createCar(brand: Brands) {
     return `${brand} car`;
   }
   // TODO: check if autocomplete works before and after the fix
+  // it works
   const car = createCar("BMW");
 }
 exercise50();
@@ -71,11 +73,16 @@ exercise50();
 function exercise51() {
   // Use satisfies constraint
   // TODO: create a tuple type that represents a 3d point
-  type TPoint = [];
-  // TODO: create a type that represents a 3d shapes (key is a string, value is an array of 3d points)
-  type TShapes = {};
+  type TPoint = [x:number, y:number, z:number];
+  type ShapeStrings = "circle" | "square"
 
-  const shapes: TShapes = {
+  // TODO: create a type that represents a 3d shapes (key is a string, value is an array of 3d points)
+
+  type TShapes = {
+    [key in ShapeStrings | (string & {})]:TPoint[]
+  }
+
+  const shapes = {
     circle: [
       [1, 2, 3],
       [4, 5, 6],
@@ -85,14 +92,17 @@ function exercise51() {
       [1, 2, 3],
       [4, 5, 6],
     ],
-  };
+    triangle:[
+      [1, 2, 3],
+    ]
+  } satisfies TShapes;
 
   // TODO: create a function that takes a list points and prints them into console
   function drawShape(points: TPoint[]) {
     console.log(points);
   }
 
-  // drawShape(shapes.circle123); // TOOD: uncomment and fix this to have compile check error, using satisfies constraint
+  drawShape(shapes.triangle); // TODO: uncomment and fix this to have compile check error, using satisfies constraint
 }
 exercise51();
 
